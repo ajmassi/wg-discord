d23192ee848765e9948f027c91c579f8a4c229ac
ajmassi <alexjmassi@gmail.com>
2023-01-07 19:15:21 -0500

    Add formatting helpers

---
+++ Makefile
@@ -0,0 +1,21 @@
+.PHONY: format, isort, flake8, black, bandit, test
+format: isort flake8 black bandit
+
+isort:
+	$(info ---------- ISORT ----------)
+	poetry run isort .
+
+flake8:
+	$(info ---------- FLAKE8 ----------)
+	poetry run flake8 . --exclude=.pytest_cache,.github,.venv \
+	    --count --select=B,C,E,F,W,T4,B9 --max-complexity=18 \
+	    --ignore=B950,E402,E203,E266,E501,W503,F403,F401 \
+	    --show-source --statistics
+
+black:
+	$(info ---------- BLACK ----------)
+	poetry run black .
+
+bandit:
+	$(info ---------- BANDIT ----------)
+	poetry run bandit -c "pyproject.toml" --recursive .
--- discord_wireguard.py
+++ discord_wireguard.py
@@ -1,9 +1,9 @@
-import binascii
-import lightbulb
 import base64
+import binascii
 import os
 import time
 
+import lightbulb
 
 bot = lightbulb.BotApp(token=os.environ["BOT_TOKEN"])
 
@@ -11,7 +11,7 @@ bot = lightbulb.BotApp(token=os.environ["BOT_TOKEN"])
 def validate_public_key(key):
     # Best we can do here for input validation is check that our received key is a 32-byte string
     if key is not None and len(base64.b64decode(key)) != 32:
-        raise ValueError("Provided value is not a valid Wireguard Public Key.")
+        raise ValueError(f"Provided value [{key}] is not a valid Wireguard Public Key.")
 
 
 @bot.command()
@@ -23,7 +23,7 @@ async def echo(ctx: lightbulb.Context) -> None:
         validate_public_key(ctx.options.key)
         await ctx.author.send(ctx.options.key)
     except binascii.Error:
-        await ctx.author.send(f"ERROR: Invalid Wireguard Public Key")
+        await ctx.author.send("ERROR: Invalid Wireguard Public Key")
     except ValueError as e:
         await ctx.author.send(f"ERROR: {e}")
     finally:
