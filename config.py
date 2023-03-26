9e6557f1ed2a79dd889c6b2ab4d16be0b76016c7
ajmassi <alexjmassi@gmail.com>
2023-03-26 03:36:52 -0400

    Add pydantic for envvar parsing

---
+++ config.py
@@ -0,0 +1,52 @@
+from pydantic import BaseSettings, Field, SecretStr
+from pydantic import IPvAnyNetwork
+from typing import Any, Optional, List
+import ipaddress
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class WireGuardSettings(BaseSettings):
+    # Discord Guild/Server WireGuard vars
+    # Required
+    guild_interface_address: IPvAnyNetwork = Field(...)
+    guild_interface_listen_port: int = Field(..., ge=1, le=65535)
+    # Optional
+    guild_save_config: Optional[bool] = Field(False)
+    guild_interface_reserved_network_addresses: Optional[List[IPvAnyNetwork]]
+    guild_interface_dns: Optional[List[IPvAnyNetwork]]
+    # TODO guild_interface_table:
+    # TODO guild_interface_mtu:
+    guild_pre_up: Optional[str]
+    guild_post_up: Optional[str]
+    guild_pre_down: Optional[str]
+    guild_post_down: Optional[str]
+
+    # Discord User Wireguard vars
+    # Required
+    # user_endpoint:
+    user_allowed_ips: List[IPvAnyNetwork]
+    # Optional
+    # TODO user_persistent_keep_alive:
+
+    class Config:
+        @classmethod
+        def cast_ip(cls, raw_ip: str) -> ipaddress.IPv4Network | ipaddress.IPv6Network | None:
+            try:
+                return ipaddress.ip_network(raw_ip, strict=False)
+            except ValueError:
+                log.error(f"Unable to cast {raw_ip} as IPv6, verify the value is a valid network address.")
+            return None
+
+        @classmethod
+        def parse_env_var(cls, field_name: str, raw_val: str) -> Any:
+            if field_name in ('guild_interface_reserved_network_addresses', 'guild_interface_dns', 'user_allowed_ips'):
+                # Cast each list member and create list of valid IPs
+                return [ip for raw_ip in raw_val.split(',') if (ip := cls.cast_ip(raw_ip)) is not None]
+            return cls.json_loads(raw_val)
+
+        env_prefix = ""
+        case_sensitive = False
+        env_file = ".env"
+        env_file_encoding = "utf-8"
--- discord_wireguard.py
+++ discord_wireguard.py
@@ -2,6 +2,7 @@ import asyncio
 import base64
 import binascii
 import ipaddress
+import logging
 import os
 
 import hikari
@@ -10,6 +11,7 @@ import wgconfig
 from dotenv import load_dotenv
 
 load_dotenv()
+log = logging.getLogger(__name__)
 bot = lightbulb.BotApp(
     token=os.environ["BOT_TOKEN"],
     intents=hikari.Intents.GUILD_MESSAGES | hikari.Intents.DM_MESSAGES,
@@ -20,21 +22,20 @@ wg_config.read_file()
 
 async def get_available_ips():
     network = set()
-    if os.getenv("INTERFACE_ADDRESS"):
-        network = ipaddress.ip_network("INTERFACE_ADDRESS")
+    if os.getenv("GUILD_INTERFACE_ADDRESS"):
+        network = ipaddress.ip_network("GUILD_INTERFACE_ADDRESS")
     else:
-        print("Interface Address required")
-        exit()
+        raise ValueError("GUILD_INTERFACE_ADDRESS is required")
 
     reserved_ips = set()
-    if os.getenv("INTERFACE_RESERVED_NETWORK_ADDRESSES"):
-        for ip in os.getenv("INTERFACE_RESERVED_NETWORK_ADDRESSES").split(","):
+    if os.getenv("GUILD_INTERFACE_RESERVED_NETWORK_ADDRESSES"):
+        for ip in os.getenv("GUILD_INTERFACE_RESERVED_NETWORK_ADDRESSES").split(","):
             try:
                 reserved_ips.update([ipaddress.ip_address(ip.strip())])
             except ValueError as e:
-                print(e)
+                log.warning(f"Invalid IP address in config file: {e}")
 
-    # Collect IPs defined in wireguard conf
+    # Collect IPs defined in WireGuard conf
     claimed_ips = set()
     for _, v in wg_config.peers.items():
         claimed_ips.update(ipaddress.ip_network(v.get("AllowedIPs")).hosts())
@@ -45,19 +46,19 @@ async def get_available_ips():
 async def validate_public_key(key):
     # Best we can do here for input validation is check that our received key is a 32-byte string
     if key is not None and len(base64.b64decode(key)) != 32:
-        raise ValueError(f"Provided value [{key}] is not a valid Wireguard public key.")
+        raise ValueError(f"Provided value [{key}] is not a valid WireGuard public key.")
 
 
 async def process_wireguard_config(ctx, user_id, key):
     config_created = False
 
-    # Check if a different is already using the provided key pair
+    # Check if a different user already registered the provided key pair
     #  The intent is that we don't want a user to be able to take over an existing connection
     if key in wg_config.peers:
         if (
             peer_id := wg_config.get_peer(key, include_details=True).get("_rawdata")[0]
         ) and peer_id.startswith("#"):
-            if peer_id[1::] == user_id.__str__():
+            if peer_id[1::] == user_id:
                 await ctx.author.send("Your public key is already configured.")
                 config_created = True
             else:
@@ -66,16 +67,23 @@ async def process_wireguard_config(ctx, user_id, key):
                 )
         else:
             # TODO work on error text, maybe also send alert to caller
-            print(
+            log.info(
                 "Config appears to be modified or created by a different tool, cannot update"
             )
     else:
+        # If the wg key is not already in use, check for and remove previous user configuration and create new one
         for k, v in wg_config.peers.items():
             if v.get("_rawdata")[0] == user_id:
                 wg_config.del_peer(k)
                 break
 
         wg_config.add_peer(key, f"#{user_id}")
+        wg_config.add_attr(
+            key,
+            "Endpoint",
+            "wg.example.com:51820",
+            "# Added for demonstration purposes",
+        )
 
         config_created = True
 
@@ -84,19 +92,22 @@ async def process_wireguard_config(ctx, user_id, key):
 
 
 @bot.command()
-@lightbulb.option("key", "Your Wireguard public key")
-@lightbulb.command("register", "Register yourself with the Wireguard tunnel.")
+@lightbulb.option("key", "Your WireGuard public key")
+@lightbulb.command("register", "Register yourself with the WireGuard tunnel.")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def echo(ctx: lightbulb.Context) -> None:
     try:
         await validate_public_key(ctx.options.key)
-        await process_wireguard_config(ctx, ctx.user.id, ctx.options.key)
+        await process_wireguard_config(ctx, ctx.user.id.__str__(), ctx.options.key)
     except binascii.Error:
-        await ctx.author.send("ERROR: Invalid Wireguard public key")
+        log.error(f"Invalid WireGuard public key: {ctx.options.key}")
+        await ctx.author.send("ERROR: Invalid WireGuard public key")
     except ValueError as e:
+        log.error(e)
         await ctx.author.send(f"ERROR: {e}")
     finally:
         # TODO context does not clear registration message when request sent in dms
+        log.info(f"User [{ctx.user.id.__str__()}] registered successfully with key [{ctx.options.key}]")
         await ctx.respond("Thanks for registering!\nReply sent to your DMs.")
         await asyncio.sleep(5)
         await ctx.delete_last_response()
