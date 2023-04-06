df76af3495574127bd9647e7a0f6676872b012e1
ajmassi <alexjmassi@gmail.com>
2023-04-06 01:35:17 -0400

    Update subprocess to call correctly

--- wg_control.py
+++ wg_control.py
@@ -6,11 +6,11 @@ from config import conf
 
 
 def hot_reload_wgconf():
-    conf_file = os.path.basename(conf.wireguard_config_path)
+    conf_file = os.path.basename(os.path.splitext(conf.wireguard_config_path)[0])
+    proc_str = "wg syncconf {0} <(wg-quick strip {0})".format(shlex.quote(conf_file))
     try:
         subprocess.run(  # nosec B603
-            shlex.split(f"wg syncconf {conf_file} <(wg-quick strip {conf_file})"),
-            check=True,
+            proc_str, shell=True, executable="/bin/bash", check=True
         )
     except subprocess.CalledProcessError as e:
         print(e)
