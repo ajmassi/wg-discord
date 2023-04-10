f21e7e908a2feb6b643b77a1e57efa39c7b51162
Charles Fleming <charlesabrahamfleming@gmail.com>
2023-04-10 02:42:14 -0400

    Fixing linting issues

--- wg_control.py
+++ wg_control.py
@@ -2,6 +2,7 @@ import os
 import shlex
 import subprocess  # nosec B404
 
+
 def hot_reload_wgconf(wireguard_config_path):
     conf_file = os.path.basename(os.path.splitext(wireguard_config_path)[0])
     proc_str = "wg syncconf {0} <(wg-quick strip {0})".format(shlex.quote(conf_file))
